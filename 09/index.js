17f43a0a2969cd82fb490ef60b99e6633dae5efe
Deepak21112005 <deepakkumarpanigrahi555@gmail.com>
2025-11-05 20:21:09 +0530

    upto 09_http methods

---
+++ 09/index.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+
+const myServer = http.createServer((req, res) => {
+
+    if (req.url === "/favicon.ico")
+        return res.end();
+
+    const log = `${Date.now()}---${req.method}---${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
+    fs.appendFile("log.txt", log, (err, data) => {
+        switch (myUrl.pathname) {
+
+            case "/":
+                if(req.method === "GET")
+                    res.end("Home Page");
+                break;
+
+            case "/about":
+                const username = myUrl.query.myname;
+                res.end(`Hy ${username}`);
+                break;
+
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end(`Here is the result for ${search}`);
+                break;
+
+            case "/signup":
+                if(req.method === 'GET')
+                    res.end("This is a SIGNUP form");
+                elseif(req.method === 'POST')
+                    res.end("SUCCESS");
+                break;
+                
+            default:
+                res.end("404 not found");
+        }
+    });
+});
+
+
+myServer.listen(8000, () => console.log("Server Started"));
